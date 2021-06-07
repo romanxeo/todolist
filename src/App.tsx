d7ec03376c7ad72e9fabab2a1b5a7c3543ac0f5c
romanxeo <romanxeo@gmail.com>
2021-06-07 18:58:00 +0300

    Commit3

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export type FilterValuesType = "all" | "active" | "completed";
 function App() {
 
     let [tasks, setTasks] = useState([
-        { id: v1(), title: "HTML&CSS", isDone: true },
+        { id: v1(), title: "HTML&CSS1", isDone: true },
         { id: v1(), title: "JS", isDone: true },
         { id: v1(), title: "ReactJS", isDone: false },
         { id: v1(), title: "Rest API", isDone: false },
