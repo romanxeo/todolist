38d074e23aa71bdaaa4c6f9c9f4438fde5842bc6
romanxeo <romanxeo@gmail.com>
2021-07-06 18:55:36 +0300

    Commit7

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
         [todoListID_1]: [{id: v1(), title: "HTML&CSS", isDone: true},
             {id: v1(), title: "JS", isDone: true},
             {id: v1(), title: "ReactJS", isDone: false},
-            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "Rest API1", isDone: false},
             {id: v1(), title: "GraphQL", isDone: false}],
         [todoListID_2]: [{id: v1(), title: "books", isDone: false},
             {id: v1(), title: "notebook", isDone: false},
